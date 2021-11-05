f73a8311dc3e6a76f661ea189ac24b7ba9caf85e
KonstantinBondarenko <constantinebond.new@gmail.com>
2021-11-05 12:10:41 +0300

    Init develop

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 // const Koa = require('koa');
 import * as Koa from 'koa';
+import * as Router from "koa-router";
 const app = new Koa();
+
+
 
 app.use(async ctx => {
     ctx.body = 'Hello World!'
 });
 
 app.listen(4000, () => {
-    console.log('Server started!')
+    console.log('Server started')
 }); 
